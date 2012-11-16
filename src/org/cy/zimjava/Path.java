90387eee9d72b460b9d3d8389326097e047a34f8
Cy Brown <cy@cybrown.fr>
2012-11-16 19:59:07 +0100

    Modified path of util classes from zimjava

---
+++ src/org/cy/zimjava/Path.java
@@ -0,0 +1,118 @@
+package org.cy.zimjava;
+
+import java.util.LinkedList;
+
+/**
+ * Path utility class.
+ * @author sigh
+ *
+ */
+public class Path implements Cloneable {
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (String name: this.path) {
+			sb.append("/");
+			sb.append(name);
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 * Exports path to an absolute path to file from root.
+	 * @param root
+	 * @return
+	 */
+	public String toFilePath(String root) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(root);
+		for (String name: this.path) {
+			sb.append("/");
+			sb.append(name);
+		}
+		sb.append(".txt");
+		return sb.toString().replace(" ", "_");
+	}
+	
+	/**
+	 * Exports path to a folder on filesystem from root.
+	 * @param root
+	 * @return
+	 */
+	public String toDirPath(String root) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(root);
+		for (String name: this.path) {
+			sb.append("/");
+			sb.append(name);
+		}
+		return sb.toString().replace(" ", "_");
+	}
+	
+	public Path clone() {
+		Path res = new Path();
+		for (String name: this.path) {
+			res.add(name);
+		}
+		return res;
+	}
+	
+	private LinkedList<String> path;
+	
+	public Path() {
+		this.path = new LinkedList<String>();
+	}
+	
+	public Path add(String s) {
+		this.path.add(s.replace("_", " "));
+		return this;
+	}
+	
+	public boolean equals(Path p) {
+		if (this.path.size() != p.path.size())
+			return false;
+		for (int i = 0; i < this.path.size(); i++) {
+			if (!this.path.get(i).equals(p.path.get(i)))
+				return false;
+		}
+		return true;
+	}
+	
+	public String getBaseName() {
+		return this.path.getLast();
+	}
+	
+	public LinkedList<String> getPath() {
+		return this.path;
+	}
+
+	/**
+	 * Set path to a string containing a zim path.
+	 * If zimpath is absolute, the path is cleared before.
+	 * If zimpath is relative, the path is used as a current path.
+	 * @param zimpath
+	 * @return
+	 */
+	public Path fromZimPath(String zimpath) {
+		if (zimpath.startsWith(":")) {
+			this.path.clear();
+			zimpath = zimpath.substring(1);
+		}
+		else if (zimpath.startsWith("+")) {
+			zimpath = zimpath.substring(1);
+		}
+		else if (zimpath.startsWith(".:")) {
+			zimpath = zimpath.substring(2);
+		}
+		else {
+			this.path.removeLast();
+		}
+		
+		for (String name: zimpath.split(":")) {
+			if ((name == null) || (name.length() == 0))
+				continue;
+			this.add(name);
+		}
+		return this;
+	}
+}
---
+++ src/org/cy/zimjava/ZimSyntax.java
@@ -0,0 +1,47 @@
+package org.cy.zimjava;
+
+import java.util.LinkedList;
+
+public class ZimSyntax {
+	private static final String STYLE = "a {text-decoration: none } a:hover {text-decoration: underline } a:active {text-decoration: underline } strike {color: grey } u {text-decoration: none; background-color: yellow } tt {color: #2e3436; } pre {color: #2e3436; margin-left: 20px } h1 {text-decoration: underline; color: #4e9a06 } h2 {color: #4e9a06 } h3 {color: #4e9a06 } h4 {color: #4e9a06 } h5 {color: #4e9a06 }";
+	static class Replacement {
+		public Replacement(String rg, String rp) {
+			this.regex = rg;
+			this.replace = rp;
+		}
+		public String regex;
+		public String replace;
+	}
+	
+	public static String toHtml(String in) {
+		String res;
+		
+		LinkedList<Replacement> r = new LinkedList<Replacement>();
+		r.add(new Replacement("&", "&amp;"));
+		r.add(new Replacement("<", "&lt;"));
+		r.add(new Replacement(">", "&gt;"));
+        r.add(new Replacement("__(.*?)__", "<u>$1</u>"));
+        r.add(new Replacement("\\*\\*(.*?)\\*\\*", "<b>$1</b>"));
+        r.add(new Replacement("//(.*?)//", "<i>$1</i>"));
+        r.add(new Replacement("(^|\n)\\* (.*)", "$1<li>$2</li>"));
+        r.add(new Replacement("(^|\n)======(.*?)(======)?((\r?\n)|$)", "$1<h1>$2</h1>"));
+        r.add(new Replacement("(^|\n)=====(.*?)(=====)?((\r?\n)|$)", "$1<h2>$2</h2>"));
+        r.add(new Replacement("(^|\n)====(.*?)(====)?((\r?\n)|$)", "$1<h3>$2</h3>"));
+        r.add(new Replacement("(^|\n)===(.*?)(===)?((\r?\n)|$)", "$1<h4>$2</h4>"));
+        r.add(new Replacement("(^|\n)==(.*?)(==)?((\r?\n)|$)", "$1<h5>$2</h5>"));
+        r.add(new Replacement("(^|\n)=(.*?)(=)?((\r?\n)|$)", "$1<h6>$2</h6>"));
+        r.add(new Replacement("\\[\\[(.*?)\\|(.*?)\\]\\]", "<a href=\"$1\">$2</a>"));
+        r.add(new Replacement("\\[\\[(.*?)\\]\\]", "<a href=\"$1\">$1</a>"));
+        r.add(new Replacement("\r?\n", "<br>"));
+		
+		res = in;
+		for (Replacement rpl: r) {
+			res = res.replaceAll(rpl.regex, rpl.replace);
+		}
+		
+		res = "<html><head><style>" + STYLE + "</style></head><body>" + res + "</body></html>";
+		
+		return res;
+	}
+	
+}
